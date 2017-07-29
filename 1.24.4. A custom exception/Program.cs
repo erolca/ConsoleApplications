5c62384c1ae73d32b5ba4e115558ea3eb1a2711b
erolcalisgan <erolcalisgan@hotmail.com>
2017-07-29 10:31:18 +0300

    A custom exception Added

---
+++ 1.24.4. A custom exception/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class CustomException : ApplicationException
+{
+    public CustomException(string Message) : base(Message)
+    {
+        this.HelpLink = "See the Readme.txt file";
+        this.Source = "My Program";
+    }
+}
+
+class MainClass
+{
+    public static void Main()
+    {
+        try
+        {
+            Console.WriteLine("Throwing a new CustomException object");
+            throw new CustomException("My CustomException message");
+        }
+        catch (CustomException e)
+        {
+            Console.WriteLine("HelpLink = " + e.HelpLink);
+            Console.WriteLine("Message = " + e.Message);
+            Console.WriteLine("Source = " + e.Source);
+            Console.WriteLine("StackTrace = " + e.StackTrace);
+            Console.WriteLine("TargetSite = " + e.TargetSite);
+        }
+    }
+}
