a38076a41c18dad2dcfab399719af9010cd8ac72
FxFn1131 <FxFn1131@DARKSIDE>
2017-09-01 23:31:58 +0300

    try catch when

---
+++ trycatch-When/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace trycatch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+
+                Console.WriteLine("Lutfen Sayi Giriniz: ");
+
+                int sayi = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(sayi + "girdiniz.");
+
+            }
+            catch (Exception ex) when(ex.Source== "mscorlib")
+            {
+
+                Console.WriteLine(ex.Message);
+            }
+
+
+        }
+    }
+}
